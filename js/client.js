51a52a10596a4829d1b17f7593ab57e6c0e9f575
Bastian Hofmann <bhofmann@vz.net>
2011-04-17 18:32:21 +0200

    call to status net timeline

--- js/client.js
+++ js/client.js
@@ -1,6 +1,12 @@
 var app = Sammy('#main', function() {
     this.get('', function() {
-      this.$element() // $('#main')
+        $.ajax({
+          url: 'http://dev.status.net:8080/index.php/api/statuses/user_timeline/bastian.json',
+          success: function(response) {
+              console.log(response);
+          }
+        });
+        this.$element() // $('#main')
           .html('A new route!');
     });
 });
