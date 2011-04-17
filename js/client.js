d0c04b9489951a4fa45e3d0147c36d4d837bed41
Bastian Hofmann <bhofmann@vz.net>
2011-04-17 20:14:38 +0200

    added mustace template

--- js/client.js
+++ js/client.js
@@ -1,13 +1,18 @@
 var app = Sammy('#main', function() {
+    this.use(Sammy.Mustache, 'ms');
     this.get('', function() {
+        this.trigger('getFeed');
+    });
+    this.bind('getFeed', function() {
+        var that = this;
         $.ajax({
           url: 'http://status.net.xyz:8061/index.php/api/statuses/user_timeline/bastian.json',
           success: function(response) {
               console.log(response);
+              that.feed = response;
+              that.partial('js/templates/feed.ms');
           }
         });
-        this.$element() // $('#main')
-          .html('A new route!');
     });
 });
 
