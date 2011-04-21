a9e8b1acdd4489d099477cf2dd3e82c696cb25ea
Bastian Hofmann <bhofmann@vz.net>
2011-04-21 17:01:52 +0200

    log in with oauth2

--- js/client.js
+++ js/client.js
@@ -3,12 +3,19 @@ var app = Sammy('#main', function() {
     this.get('', function() {
         this.trigger('getFeed');
     });
+    this.get('#Login', function() {
+       var consumerKey = '2e70313dfb8a673a1ad84f441595b38e';
+       window.open('http://status.net.xyz:8061/index.php/api/oauth2/authorize?response_toke=token&client_id=' + consumerKey);
+    });
     this.bind('getFeed', function() {
+        if (! oauth2.authParameters || ! oauth2.authParameters['access_token']) {
+            this.redirect('#Login');
+            return;
+        }
         var that = this;
         $.ajax({
-          url: 'http://status.net.xyz:8061/index.php/api/statuses/home_timeline.json',
+          url: 'http://status.net.xyz:8061/index.php/api/statuses/home_timeline.json?oauth_token=' + oauth2.authParameters['access_token'],
           success: function(response) {
-              console.log(response);
               that.feed = response;
               that.partial('js/templates/feed.ms');
           }
@@ -19,3 +26,18 @@ var app = Sammy('#main', function() {
 jQuery(function() {
     app.run();
 });
+
+var oauth2 = {
+    authParameters: {},
+    storeAccessToken : function(fragment) {
+        fragment = fragment.split('+').join('%252b');
+        fragment = fragment.split('&');
+        for (var i = 0; i < fragment.length; i++) {
+            var ix = fragment[i].indexOf('=');
+            if (ix > 0) {
+                oauth2.authParameters[fragment[i].substr(0, ix)] = decodeURIComponent(fragment[i].substr(ix + 1));
+            }
+        }
+        app.trigger('getFeed');
+    }
+}
