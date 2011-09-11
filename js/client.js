01a6c3b77d168568bc96f0190ec8d480f9cadde6
Bastian Hofmann <bashofmann@googlemail.com>
2011-09-11 03:28:03 +0200

    log in with oauth2
    Conflicts:
    
    	js/client.js

--- js/client.js
+++ js/client.js
@@ -3,12 +3,19 @@ var app = Sammy('#main', function() {
     this.get('', function() {
         this.trigger('getFeed');
     });
+    this.get('#Login', function() {
+       var consumerKey = '71b454c797a58e8de5df33137e95cb8c';
+       window.open('http://dev.status.net:8080/index.php/api/oauth2/authorize?response_toke=token&client_id=' + consumerKey);
+    });
     this.bind('getFeed', function() {
+        if (! oauth2.authParameters || ! oauth2.authParameters['access_token']) {
+            this.redirect('#Login');
+            return;
+        }
         var that = this;
         $.ajax({
-          url: 'http://dev.status.net:8080/index.php/api/statuses/home_timeline.json',
+          url: 'http://dev.status.net:8080/index.php/api/statuses/home_timeline.json?oauth_token=' + oauth2.authParameters['access_token'],
           success: function(response) {
-              console.log(response);
               that.feed = response;
               that.partial('js/templates/feed.ms');
           }
@@ -19,3 +26,18 @@ var app = Sammy('#main', function() {
 jQuery(function() {
     app.run();
 });
+
+var oauth2 = {
+    authParameters: {},
+    storeAccessToken : function(fragment) {
+        fragment = fragment.split('+').join('%252b');
+        fragment = fragment.split('&');
+        for (var i = 0; i < fragment.length; i++) {
+            var ix = fragment[i].indexOf('=');
+            if (ix > 0) {
+                oauth2.authParameters[fragment[i].substr(0, ix)] = decodeURIComponent(fragment[i].substr(ix + 1));
+            }
+        }
+        app.trigger('getFeed');
+    }
+}
