ee30e8cd119cd2bd3965d599e95f08ce01b8141d
Bastian Hofmann <bashofmann@googlemail.com>
2011-09-11 03:26:51 +0200

    unauthorized request
    Conflicts:
    
    	js/client.js

--- js/client.js
+++ js/client.js
@@ -6,7 +6,7 @@ var app = Sammy('#main', function() {
     this.bind('getFeed', function() {
         var that = this;
         $.ajax({
-          url: 'http://dev.status.net:8080/index.php/api/statuses/user_timeline/bastian.json',
+          url: 'http://dev.status.net:8080/index.php/api/statuses/home_timeline.json',
           success: function(response) {
               console.log(response);
               that.feed = response;
